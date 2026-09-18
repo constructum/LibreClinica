acc200f2f6a3b81aa3b7787fd03b9bce1781b92a
Giuseppe Del Castillo <constructum@web.de>
2026-09-18 19:46:47 +0200

    First working implementation of migrated "databaseChangeLogs" table (defined within "AuditDatabaseServlet").

--- web/src/main/java/org/akaza/openclinica/control/admin/AuditDatabaseServlet.java
+++ web/src/main/java/org/akaza/openclinica/control/admin/AuditDatabaseServlet.java
@@ -10,15 +10,38 @@
 package org.akaza.openclinica.control.admin;
 
 import static org.jmesa.facade.TableFacadeFactory.createTableFacade;
+import static org.akaza.openclinica.lctable.LCTableColumnDef.textCol;
+import static org.akaza.openclinica.lctable.LCTableFilterDef.textFilter;
+import static org.akaza.openclinica.lctable.LCTableParams.PARAM_MAX_ROWS;
+import static org.akaza.openclinica.lctable.LCTableParams.PARAM_PAGE;
+import static org.akaza.openclinica.lctable.LCTableParams.PARAM_SHOW_HIDDEN_COLS;
+import static org.akaza.openclinica.lctable.LCTableParams.PARAM_SORT_DIR;
+import static org.akaza.openclinica.lctable.LCTableParams.PARAM_SORT_PROP;
+import static org.akaza.openclinica.lctable.LCTableText.key;
+import static org.akaza.openclinica.lctable.LCTableUtil.NO_HX_TRIGGER;
+import static org.akaza.openclinica.lctable.LCTableUtil.TIMESTAMP_FILTER_FOR_HTML_VALIDATION;
+import static org.akaza.openclinica.lctable.LCTableUtil.TIMESTAMP_FILTER_MESSAGE;
+import static org.akaza.openclinica.lctable.LCTableUtil.hxGetAttrs;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.function.Function;
 
 import org.akaza.openclinica.control.SpringServletAccess;
 import org.akaza.openclinica.control.core.SecureController;
 import org.akaza.openclinica.dao.hibernate.DatabaseChangeLogDao;
 import org.akaza.openclinica.domain.technicaladmin.DatabaseChangeLogBean;
 import org.akaza.openclinica.i18n.core.LocaleResolver;
+import org.akaza.openclinica.lctable.LCTable;
+import org.akaza.openclinica.lctable.LCTableColumnDef;
+import org.akaza.openclinica.lctable.LCTableContext;
+import org.akaza.openclinica.lctable.LCTableData;
+import org.akaza.openclinica.lctable.LCTableParams;
+import org.akaza.openclinica.lctable.LCTableUtil;
 import org.akaza.openclinica.view.Page;
 import org.akaza.openclinica.web.InsufficientPermissionException;
 import org.jmesa.facade.TableFacade;
@@ -26,6 +49,7 @@ import org.jmesa.view.editor.DateCellEditor;
 import org.jmesa.view.html.component.HtmlColumn;
 import org.jmesa.view.html.component.HtmlRow;
 import org.jmesa.view.html.component.HtmlTable;
+import org.xmlet.htmlapifaster.Div;
 
 /**
  * Servlet for creating a user account.
@@ -35,6 +59,7 @@ import org.jmesa.view.html.component.HtmlTable;
 public class AuditDatabaseServlet extends SecureController {
 
     private static final long serialVersionUID = 1L;
+    private static final String TABLE_NAME = "databaseChangeLogs";
 
     // < ResourceBundle restext;
     Locale locale;
@@ -60,10 +85,140 @@ public class AuditDatabaseServlet extends SecureController {
 
     @Override
     protected void processRequest() throws Exception {
-        String auditDatabaseHtml = renderAuditDatabaseTable(getDatabaseChangeLogDao().findAll());
-        request.setAttribute("auditDatabaseHtml", auditDatabaseHtml);
-        forwardPage(Page.AUDIT_DATABASE);
+        List<DatabaseChangeLogBean> databaseChangeLogs = getDatabaseChangeLogDao().findAll();
+        String lcTableRendering = System.getenv("LC_TABLE_RENDERING");
+        if (lcTableRendering != null && lcTableRendering.equalsIgnoreCase("jmesa")) {
+            request.setAttribute("tableRenderingMode", "jmesa");
+            request.setAttribute("auditDatabaseHtml", renderAuditDatabaseTable(databaseChangeLogs));
+            forwardPage(Page.AUDIT_DATABASE);
+        } else {
+            request.setAttribute("tableRenderingMode", "htmlflow");
+            String auditDatabaseHtml = renderAuditDatabaseLCTable(databaseChangeLogs);
+            response.addHeader("Vary", "HX-Request");
+            if (request.getHeader("HX-Request") != null) {
+                response.setContentType("text/html;charset=UTF-8");
+                response.getWriter().write(auditDatabaseHtml);
+                response.getWriter().flush();
+            } else {
+                request.setAttribute("auditDatabaseHtml", auditDatabaseHtml);
+                forwardPage(Page.AUDIT_DATABASE);
+            }
+        }
 
+    }
+
+    String renderAuditDatabaseLCTable(List<DatabaseChangeLogBean> databaseChangeLogs) {
+        List<LCTableColumnDef<DatabaseChangeLogBean>> columns = List.of(
+            textCol("id",           key("Id"),            "id",            0, DatabaseChangeLogBean::getId),
+            textCol("author",       key("author"),        "author",        0, DatabaseChangeLogBean::getAuthor),
+            textCol("fileName",     key("file_name"),     "file-name",     0, DatabaseChangeLogBean::getFileName),
+            textCol("dataExecuted", key("date_executed"), "date-executed", 0,
+                textFilter(TIMESTAMP_FILTER_FOR_HTML_VALIDATION, TIMESTAMP_FILTER_MESSAGE),
+                DatabaseChangeLogBean::getDataExecuted, LCTableUtil::timestampToString),
+            textCol("md5Sum",       key("md5_sum"),       "md5-sum",       0, DatabaseChangeLogBean::getMd5Sum),
+            textCol("description",  key("description"),   "description",   0, DatabaseChangeLogBean::getDescription),
+            textCol("comments",     key("comments"),      "comments",      0, DatabaseChangeLogBean::getComments),
+            textCol("tag",          key("tag"),           "tag",           0, DatabaseChangeLogBean::getTag),
+            textCol("liquibase",    key("liquibase"),     "liquibase",     0, DatabaseChangeLogBean::getLiquibase)
+        );
+        LCTable<DatabaseChangeLogBean> table = new LCTable<>(TABLE_NAME, columns,
+            params -> fetchAuditDatabaseData(databaseChangeLogs, params));
+        table.addCustomToolbarControl(this::renderClearFilterControl);
+
+        LCTableParams params = new LCTableParams(request.getQueryString(), table);
+        return table.render(request.getRequestURI(), params, request.getContextPath(), LocaleResolver.getLocale(request));
+    }
+
+    private LCTableData<DatabaseChangeLogBean> fetchAuditDatabaseData(
+            List<DatabaseChangeLogBean> databaseChangeLogs, LCTableParams params) {
+        List<DatabaseChangeLogBean> filteredRows = new ArrayList<>();
+        for (DatabaseChangeLogBean row : databaseChangeLogs) {
+            if (matchesFilters(row, params.filters)) {
+                filteredRows.add(row);
+            }
+        }
+
+        Comparator<DatabaseChangeLogBean> comparator = comparatorFor(params.sortProp);
+        if (comparator != null) {
+            if ("desc".equalsIgnoreCase(params.sortDir)) {
+                comparator = comparator.reversed();
+            }
+            filteredRows.sort(comparator);
+        }
+
+        int fromIndex = Math.min(params.page * params.maxRows, filteredRows.size());
+        int toIndex = Math.min(fromIndex + params.maxRows, filteredRows.size());
+        return new LCTableData<>(new ArrayList<>(filteredRows.subList(fromIndex, toIndex)), filteredRows.size());
+    }
+
+    private boolean matchesFilters(DatabaseChangeLogBean row, Map<String, String> filters) {
+        Map<String, String> values = new HashMap<>();
+        values.put("id", row.getId());
+        values.put("author", row.getAuthor());
+        values.put("fileName", row.getFileName());
+        values.put("dataExecuted", row.getDataExecuted() == null ? null : LCTableUtil.timestampToString(row.getDataExecuted()));
+        values.put("md5Sum", row.getMd5Sum());
+        values.put("description", row.getDescription());
+        values.put("comments", row.getComments());
+        values.put("tag", row.getTag());
+        values.put("liquibase", row.getLiquibase());
+
+        return filters.entrySet().stream().allMatch(filter -> {
+            String value = values.get(filter.getKey());
+            return value != null && value.toLowerCase(Locale.ROOT).contains(filter.getValue().toLowerCase(Locale.ROOT));
+        });
+    }
+
+    private Comparator<DatabaseChangeLogBean> comparatorFor(String property) {
+        if ("id".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getId);
+        }
+        if ("author".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getAuthor);
+        }
+        if ("fileName".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getFileName);
+        }
+        if ("dataExecuted".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getDataExecuted);
+        }
+        if ("md5Sum".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getMd5Sum);
+        }
+        if ("description".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getDescription);
+        }
+        if ("comments".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getComments);
+        }
+        if ("tag".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getTag);
+        }
+        if ("liquibase".equals(property)) {
+            return comparingNullable(DatabaseChangeLogBean::getLiquibase);
+        }
+        return null;
+    }
+
+    private static <U extends Comparable<? super U>> Comparator<DatabaseChangeLogBean> comparingNullable(
+            Function<DatabaseChangeLogBean, U> extractor) {
+        return Comparator.comparing(extractor, Comparator.nullsFirst(Comparator.naturalOrder()));
+    }
+
+    private void renderClearFilterControl(Div<?> container, LCTableContext<DatabaseChangeLogBean> context) {
+        String nonFilterParams = "[name=" + PARAM_PAGE + "]"
+            + ",[name=" + PARAM_MAX_ROWS + "]"
+            + ",[name=" + PARAM_SORT_PROP + "]"
+            + ",[name=" + PARAM_SORT_DIR + "]"
+            + ",[name=" + PARAM_SHOW_HIDDEN_COLS + "]";
+        container.a()
+            .attrId(TABLE_NAME + "-clear-filter-toolbar")
+            .attrClass("text-btn")
+            .addAttr("data-testid", "clear-filter-button")
+            .addAttr("data-test-action", "clear-filter")
+            .of(hxGetAttrs(context.entityPath, nonFilterParams, "#" + TABLE_NAME + "-panel", NO_HX_TRIGGER))
+            .text(context.words.getString("table_clear_filter"))
+            .__();
     }
 
     private String renderAuditDatabaseTable(List<DatabaseChangeLogBean> databaseChangeLogs) {
--- web/src/main/webapp/WEB-INF/jsp/admin/auditDatabase.jsp
+++ web/src/main/webapp/WEB-INF/jsp/admin/auditDatabase.jsp
@@ -2,26 +2,33 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
 
+<c:choose>
+    <c:when test="${tableRenderingMode == 'jmesa'}">
+        <link rel="stylesheet" href="includes/jmesa/jmesa.css" type="text/css">
+    </c:when>
+    <c:otherwise>
+        <link rel="stylesheet" href="includes/lctable/lctable.css" type="text/css">
+    </c:otherwise>
+</c:choose>
+<c:if test="${tableRenderingMode == 'jmesa'}">
+    <script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jquery.min.js"></script>
+    <script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jquery.jmesa.js"></script>
+    <script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jmesa.js"></script>
+    <script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jquery-migrate-3.4.1.min.js"></script>
 
-<link rel="stylesheet" href="includes/jmesa/jmesa.css" type="text/css">
-<script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jquery.min.js"></script>
-<script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jquery.jmesa.js"></script>
-<script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jmesa.js"></script>
-<script type="text/JavaScript" language="JavaScript" src="includes/jmesa/jquery-migrate-3.4.1.min.js"></script> 
-
-
-<script type="text/javascript">
-    function onInvokeAction(id,action) {
-        if(id.indexOf('databaseChangeLogs') == -1)  {
-        setExportToLimit(id, '');
+    <script type="text/javascript">
+        function onInvokeAction(id,action) {
+            if(id.indexOf('databaseChangeLogs') == -1)  {
+                setExportToLimit(id, '');
+            }
+            createHiddenInputFieldsForLimitAndSubmit(id);
         }
-        createHiddenInputFieldsForLimitAndSubmit(id);
-    }
-    function onInvokeExportAction(id) {
-        var parameterString = createParameterStringForLimit(id);
-        location.href = '${pageContext.request.contextPath}/AuditDatabase;'
-    }
-</script>
+        function onInvokeExportAction(id) {
+            var parameterString = createParameterStringForLimit(id);
+            location.href = '${pageContext.request.contextPath}/AuditDatabase;'
+        }
+    </script>
+</c:if>
 
 <fmt:setBundle basename="org.akaza.openclinica.i18n.words" var="resword"/>
 
@@ -66,10 +73,17 @@
 <jsp:useBean id="now" class="java.util.Date" />
 <P><I><fmt:message key="server_time_info" bundle="${resword}"/> <fmt:formatDate value="${now}" pattern="yyyy-MM-dd hh:mm"/>.</I></P>
 <div id="auditDatabaseDiv">
-    <form  action="${pageContext.request.contextPath}/AuditDatabase">
-        <input type="hidden" name="module" value="admin">
-        ${auditDatabaseHtml}
-    </form>
+    <c:choose>
+        <c:when test="${tableRenderingMode == 'jmesa'}">
+            <form action="${pageContext.request.contextPath}/AuditDatabase">
+                <input type="hidden" name="module" value="admin">
+                ${auditDatabaseHtml}
+            </form>
+        </c:when>
+        <c:otherwise>
+            ${auditDatabaseHtml}
+        </c:otherwise>
+    </c:choose>
 </div>
 
 
@@ -88,4 +102,7 @@
         </c:import>
     </c:otherwise>
 </c:choose>
+<!-- Include everything that is needed for proper use of HTMX with LCTable -->
+<jsp:include page="../include/useLCTable.jsp"/>
+
 <jsp:include page="../include/footer.jsp"/>
